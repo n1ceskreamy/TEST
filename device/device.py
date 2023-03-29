580610fa940929ea39e2ed7f14a89a023f35f13e
Sergey Sobolev <sergey.p.sobolev@kaspersky.com>
2023-03-29 15:48:23 +0300

    bugfix for sending keys

--- device/device.py
+++ device/device.py
@@ -87,6 +87,7 @@ def cron(t):
         check_status = diagnostic()
         check_status = str(check_status)
         out_d("send_diagnostic", "Checked with status: " + check_status)
+        out_d("send_key", key)
 
 
 #генератор ключей
